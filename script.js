29d0a9cd4effa2fb064ec2b51419fd1fcc06f4e6
Kazuya Minegishi <92979252+kazuya60@users.noreply.github.com>
2024-11-28 21:25:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    function showMessage() {
+        const messageBox = document.querySelector('.human-detected');
+        if (messageBox) {
+            messageBox.classList.remove('hidden');
+        }
+    }
